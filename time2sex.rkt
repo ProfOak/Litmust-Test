0aa97fb61733f255c3092ccd5a7597e74e63474c
ProfOak <OpenProfOak@gmail.com>
2014-08-06 00:01:56 -0500

    better reflect the print
    get-seconds returns seconds, print is done elsewhere

--- time2sex.rkt
+++ time2sex.rkt
@@ -3,7 +3,7 @@
 
 ; time2sex in Racket 
 
-(define help-string "Please enter a command line arg\nHH:MM:SS\nEX:\n    time2sex 1:23:45")
+(define help-string "Please enter a command line arg\nHH:MM:SS\nEX:\n    time2sex 12:34:56")
 
 (define (printsex s) (printf "=== ~a seconds ===\n" s))
 
@@ -14,15 +14,12 @@
 (define (convert time) 
   (let ([t (map string->number (string-split time ":"))])
     (cond 
-    [(equal? 1 (length t))    ; seconds only
-     (printsex  (first t))]
-    [(equal? 2 (length t))    ; minutes:seconds
-     (printsex (get-seconds t (list 60 1)))]
-    [(equal? 3 (length t))    ; hours:minutes:seconds
-     (printsex (get-seconds t (list 3600 60 1)))])))
+    [(equal? 1 (length t)) (first t)]                            ; seconds only
+    [(equal? 2 (length t)) (get-seconds t (list 60 1))]          ; minutes:seconds
+    [(equal? 3 (length t)) (get-seconds t (list 3600 60 1))])))  ; hours:minutes:seconds
      
 ; main function
 (let ([args (current-command-line-arguments)])
   (if (equal? 1 (vector-length args))
-     (convert (vector-ref args 0))
+     (printsex (convert (vector-ref args 0)))
      (displayln help-string)))
