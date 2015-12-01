609028b26c0ad0ff74b730a90c8827f0460d7dea
ProfOak <OpenProfOak@gmail.com>
2015-12-01 02:25:16 -0600

    cond->case change in racket ver

--- time2sex.rkt
+++ time2sex.rkt
@@ -1,19 +1,19 @@
 #! /usr/bin/env racket
 #lang racket
 
-; time2sex in Racket 
+; time2sex in Racket
 
 (define (get-seconds l1 l2)
   (let ([multiplied-numbers (map * l1 l2)])
     (apply + multiplied-numbers)))
 
-(define (convert time) 
+(define (convert time)
   (let ([t (map string->number (string-split time ":"))])
-    (cond 
-    [(equal? 1 (length t)) (first t)]                            ; seconds only
-    [(equal? 2 (length t)) (get-seconds t (list 60 1))]          ; minutes:seconds
-    [(equal? 3 (length t)) (get-seconds t (list 3600 60 1))])))  ; hours:minutes:seconds
-     
+    (case (length t)
+    [(1) (first t)]                            ; seconds only
+    [(2) (get-seconds t (list 60 1))]          ; minutes:seconds
+    [(3) (get-seconds t (list 3600 60 1))])))  ; hours:minutes:seconds
+
 ; main function
 (let ([args (current-command-line-arguments)])
   (if (equal? 1 (vector-length args))
