6bcde0b7252c83f2125d4b2d2cb1b2d41abc8fce
ProfOak <OpenProfOak@gmail.com>
2015-12-01 05:11:04 -0600

    re-wrote everything, much better code golf

--- time2sex.rkt
+++ time2sex.rkt
@@ -2,20 +2,14 @@
 #lang racket
 
 ; time2sex in Racket
-
-(define (get-seconds l1 l2)
-  (let ([multiplied-numbers (map * l1 l2)])
-    (apply + multiplied-numbers)))
-
-(define (convert time)
-  (let ([t (map string->number (string-split time ":"))])
-    (case (length t)
-    [(1) (first t)]                            ; seconds only
-    [(2) (get-seconds t (list 60 1))]          ; minutes:seconds
-    [(3) (get-seconds t (list 3600 60 1))])))  ; hours:minutes:seconds
+(define (time2sex time)
+  (let ([time (map string->number (string-split time ":"))])
+    (for/sum ([t (reverse time)]
+              [s '(1 60 3600)])
+      (* t s))))
 
 ; main function
 (let ([args (current-command-line-arguments)])
   (if (equal? 1 (vector-length args))
-     (printf "=== ~a seconds ===\n"  (convert (vector-ref args 0)))
+     (printf "=== ~a seconds ===\n"  (time2sex (vector-ref args 0)))
      (displayln "Please enter a command line arg\nHH:MM:SS\nEX:\n    time2sex 12:34:56")))
