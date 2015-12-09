85d67e32088d6d2aadc7e0618560247c2d560f4b
ProfOak <OpenProfOak@gmail.com>
2015-12-08 22:11:52 -0600

    real one liner

--- time2sex_golf.py
+++ time2sex_golf.py
@@ -1,3 +1,1 @@
-#!/usr/bin/python
-import sys
-print "=== %s seconds ===" % sum( [[1,60,3600][i] * int(j) for i, j in enumerate(reversed(sys.argv[1].split(":")[:3]))] )
+import sys;print "=== %s seconds ===" % sum( [[1,60,3600][i] * int(j) for i, j in enumerate(reversed(sys.argv[1].split(":")[:3]))] )
