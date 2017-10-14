a525b203f96159baf190c223203f8231d2cb4c19
ProfOak <OpenProfOak@gmail.com>
2017-10-14 18:44:19 -0500

    fix(move): Moved haskell version to folder

--- time2sex/time2sex.hs
+++ time2sex/time2sex.hs
@@ -1,7 +1,15 @@
-import System.Environment
+import Data.List.Split    {- splitOn -}
+import System.Environment {- getArgs -}
+
+time2sex :: String -> Int
+time2sex time_str =
+    sum (zipWith (*) time seconds)
+    where
+        time = map (read::String->Int) (reverse (splitOn ":" time_str))
+        seconds = [1, 60, 3600]
 
 main = do
-    let args = ["12","34", "56"]
-    map read args :: [Int]
-    putStrLn show args :: String
+    args <- getArgs
+    let time = args !! 0
+    print (time2sex time)
 
